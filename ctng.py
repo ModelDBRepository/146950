94d7818fa6a7c8a91fd7794c5df6a03aeb052a8a
Tom Morse <tom.morse@yale.edu>
2016-05-31 17:44:38 -0400

    original model

---
+++ ctng.py
@@ -0,0 +1,69 @@
+#! /usr/bin/python
+
+import sys
+
+filename_out = None
+
+try:
+    filename = sys.argv[1]
+except:
+    print 'run as: "python %s FILENAME_IN [FILENAME_OUT] [dx] [nouniform]"' % sys.argv[0]
+    print 'if the last argument is nouniform, CTNG will not force a unique diameter at each point'
+    sys.exit(-1)
+
+lastarg = sys.argv[-1]
+if lastarg == 'nouniform':
+    nouniform = True
+    dx_loc = -2
+else:
+    nouniform = False
+    dx_loc = -1
+
+try:
+    dx = float(sys.argv[dx_loc])
+    has_dx = True
+except:
+    # default discretization
+    dx = 0.25
+    has_dx = False
+
+if len(sys.argv) > 3 or (not has_dx and len(sys.argv) == 3):
+    filename_out = sys.argv[2]
+
+from neuron import h
+from mayavi import mlab
+import geometry3d
+import time
+
+h.load_file('stdlib.hoc')
+h.load_file('import3d.hoc')
+
+cell = h.Import3d_Neurolucida3()
+cell.input(filename)
+
+start = time.time()
+tri_mesh = geometry3d.surface(cell, dx, n_soma_step=100, nouniform=nouniform)
+mlab.triangular_mesh(tri_mesh.x, tri_mesh.y, tri_mesh.z, tri_mesh.faces, color=(1, 0, 0))
+print 'time to construct mesh:', time.time() - start
+
+start = time.time()
+area = tri_mesh.area
+print 'area: ', area
+print 'time to compute area:', time.time() - start
+
+start = time.time()
+vol = tri_mesh.enclosed_volume
+print 'volume: ', vol
+print 'time to compute volume:', time.time() - start
+
+
+if filename_out:
+    triangles = tri_mesh.data
+    with open(filename_out, 'w') as output:
+        output.write('%d, %g\n' % (len(triangles), area))
+        for i in xrange(len(triangles) / 9):
+            output.write('%g, %g, %g,    %g, %g, %g,    %g, %g, %g\n' % tuple(triangles[i : i + 9]))
+
+
+mlab.show()     
+
---
+++ readme.txt
@@ -0,0 +1,48 @@
+CTNG (Constructive Tessellated Neuronal Geometry) is a tool for
+constructing a 3d tesselation of a neuron's surface from
+point-diameter data.
+
+Before use: compile the C and Cython routines:
+
+    cd geometry3d
+    python setup.py build_ext --inplace
+
+To use (for now displays the neuron, but could easily write data to a file):
+
+    python ctng.py FILENAME_IN [FILENAME_OUT] [dx]
+    
+where neuron_file is the name of the source morphology.
+
+e.g.
+
+    python ctng.py Vn03082006-0-D.ASC out.tri 0.25
+
+The first line of an output file lists the number of triangles and the area.
+
+Each triangle is listed on its own line in the following format:
+
+    x1, y1, z1,    x2, y2, z2,    x3, y3, z3
+
+Requires:
+    g++
+    cython
+    python
+        numpy
+        mayavi
+    NEURON
+
+
+Most of the figures for the CTNG paper were made using the morphology from
+
+http://neuromorpho.org/neuroMorpho/neuron_info.jsp?neuron_name=Vn03082006-0-D
+
+Changelog:
+
+2012-12-10    sphere tests for contains_surface is primary
+              bugfix for case where no output file specified
+              lowered minimum chunk width from 100 to 20 voxels
+
+2013-01-21    refactoring, bugfixes
+              surfaces now provably watertight (last change introduced holes)
+
+2013-10-05    fix to extreme points of soma (was using axis, not the soma data)
